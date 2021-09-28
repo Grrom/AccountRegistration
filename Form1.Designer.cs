2e9d0bd2833406e78b4f2a24d06c7b1a24528bda
Jerome <josejeromelalunio@gmail.com>
2021-09-29 05:32:10 +0800

    add next button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,127 +45,154 @@ namespace AccountRegistration
             this.studentNoBox = new System.Windows.Forms.TextBox();
             this.programBox = new System.Windows.Forms.TextBox();
             this.addressBox = new System.Windows.Forms.TextBox();
-
-
+            this.nextButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
-
-
+            // 
+            // studentNo
+            // 
             this.studentNo.AutoSize = true;
             this.studentNo.Location = new System.Drawing.Point(44, 29);
-            this.studentNo.Name = "Student No";
-            this.studentNo.Size = new System.Drawing.Size(38, 15);
+            this.studentNo.Name = "studentNo";
+            this.studentNo.Size = new System.Drawing.Size(67, 15);
             this.studentNo.TabIndex = 0;
             this.studentNo.Text = "Student No";
-
-
+            // 
+            // program
+            // 
             this.program.AutoSize = true;
             this.program.Location = new System.Drawing.Point(257, 29);
-            this.program.Name = "Program";
-            this.program.Size = new System.Drawing.Size(38, 15);
+            this.program.Name = "program";
+            this.program.Size = new System.Drawing.Size(53, 15);
             this.program.TabIndex = 1;
             this.program.Text = "Program";
-
-
+            // 
+            // lastName
+            // 
             this.lastName.AutoSize = true;
             this.lastName.Location = new System.Drawing.Point(44, 92);
-            this.lastName.Name = "Last Name";
-            this.lastName.Size = new System.Drawing.Size(38, 15);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(63, 15);
             this.lastName.TabIndex = 3;
             this.lastName.Text = "Last Name";
-
-
+            // 
+            // firstName
+            // 
             this.firstName.AutoSize = true;
             this.firstName.Location = new System.Drawing.Point(257, 92);
-            this.firstName.Name = "First Name";
-            this.firstName.Size = new System.Drawing.Size(38, 15);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(64, 15);
             this.firstName.TabIndex = 2;
             this.firstName.Text = "First Name";
-
-
+            // 
+            // middleName
+            // 
             this.middleName.AutoSize = true;
             this.middleName.Location = new System.Drawing.Point(469, 92);
-            this.middleName.Name = "Middle Name";
-            this.middleName.Size = new System.Drawing.Size(38, 15);
+            this.middleName.Name = "middleName";
+            this.middleName.Size = new System.Drawing.Size(79, 15);
             this.middleName.TabIndex = 4;
             this.middleName.Text = "Middle Name";
-
-
+            // 
+            // age
+            // 
             this.age.AutoSize = true;
             this.age.Location = new System.Drawing.Point(44, 159);
-            this.age.Name = "Age";
-            this.age.Size = new System.Drawing.Size(38, 15);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(28, 15);
             this.age.TabIndex = 5;
             this.age.Text = "Age";
-
-
-            this.address.AutoSize = true;
-            this.address.Location = new System.Drawing.Point(44, 225);
-            this.address.Name = "Address";
-            this.address.Size = new System.Drawing.Size(38, 15);
-            this.address.TabIndex = 6;
-            this.address.Text = "Address";
-
-
+            // 
+            // contactNo
+            // 
             this.contactNo.AutoSize = true;
             this.contactNo.Location = new System.Drawing.Point(257, 159);
-            this.contactNo.Name = "Contact No";
-            this.contactNo.Size = new System.Drawing.Size(38, 15);
+            this.contactNo.Name = "contactNo";
+            this.contactNo.Size = new System.Drawing.Size(68, 15);
             this.contactNo.TabIndex = 7;
             this.contactNo.Text = "Contact No";
-             
-             
-            this.studentNoBox.Location = new System.Drawing.Point(44, 47);
-            this.studentNoBox.Name = "textBox6";
-            this.studentNoBox.Size = new System.Drawing.Size(207, 23);
-            this.studentNoBox.TabIndex = 14;
-            
-             
-            this.programBox.Location = new System.Drawing.Point(257, 47);
-            this.programBox.Name = "textBox7";
-            this.programBox.Size = new System.Drawing.Size(313, 23);
-            this.programBox.TabIndex = 15;
-
-
-            this.lastNameBox.Location = new System.Drawing.Point(44, 110);
-            this.lastNameBox.Name = "textBox2";
-            this.lastNameBox.Size = new System.Drawing.Size(207, 23);
-            this.lastNameBox.TabIndex = 11;
-            
-
-            this.firstNameBox.Location = new System.Drawing.Point(257, 110);
-            this.firstNameBox.Name = "textBox3";
-            this.firstNameBox.Size = new System.Drawing.Size(207, 23);
-            this.firstNameBox.TabIndex = 10;
-
-
+            // 
+            // address
+            // 
+            this.address.AutoSize = true;
+            this.address.Location = new System.Drawing.Point(44, 225);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(49, 15);
+            this.address.TabIndex = 6;
+            this.address.Text = "Address";
+            // 
+            // middleNameBox
+            // 
             this.middleNameBox.Location = new System.Drawing.Point(469, 110);
-            this.middleNameBox.Name = "textBox1";
+            this.middleNameBox.Name = "middleNameBox";
             this.middleNameBox.Size = new System.Drawing.Size(207, 23);
             this.middleNameBox.TabIndex = 8;
-
-
+            // 
+            // firstNameBox
+            // 
+            this.firstNameBox.Location = new System.Drawing.Point(257, 110);
+            this.firstNameBox.Name = "firstNameBox";
+            this.firstNameBox.Size = new System.Drawing.Size(207, 23);
+            this.firstNameBox.TabIndex = 10;
+            // 
+            // lastNameBox
+            // 
+            this.lastNameBox.Location = new System.Drawing.Point(44, 110);
+            this.lastNameBox.Name = "lastNameBox";
+            this.lastNameBox.Size = new System.Drawing.Size(207, 23);
+            this.lastNameBox.TabIndex = 11;
+            // 
+            // ageBox
+            // 
             this.ageBox.Location = new System.Drawing.Point(44, 177);
-            this.ageBox.Name = "textBox4";
+            this.ageBox.Name = "ageBox";
             this.ageBox.Size = new System.Drawing.Size(207, 23);
             this.ageBox.TabIndex = 12;
-            
-             
+            // 
+            // contactBox
+            // 
             this.contactBox.Location = new System.Drawing.Point(257, 177);
-            this.contactBox.Name = "textBox5";
+            this.contactBox.Name = "contactBox";
             this.contactBox.Size = new System.Drawing.Size(207, 23);
             this.contactBox.TabIndex = 13;
-
-
+            // 
+            // studentNoBox
+            // 
+            this.studentNoBox.Location = new System.Drawing.Point(44, 47);
+            this.studentNoBox.Name = "studentNoBox";
+            this.studentNoBox.Size = new System.Drawing.Size(207, 23);
+            this.studentNoBox.TabIndex = 14;
+            // 
+            // programBox
+            // 
+            this.programBox.Location = new System.Drawing.Point(257, 47);
+            this.programBox.Name = "programBox";
+            this.programBox.Size = new System.Drawing.Size(313, 23);
+            this.programBox.TabIndex = 15;
+            // 
+            // addressBox
+            // 
             this.addressBox.Location = new System.Drawing.Point(44, 243);
             this.addressBox.Multiline = true;
-            this.addressBox.Name = "textBox8";
+            this.addressBox.Name = "addressBox";
             this.addressBox.Size = new System.Drawing.Size(708, 153);
             this.addressBox.TabIndex = 16;
-
-
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(363, 415);
+            this.nextButton.Name = "Next";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 17;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // Form1
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nextButton);
             this.Controls.Add(this.addressBox);
             this.Controls.Add(this.programBox);
             this.Controls.Add(this.studentNoBox);
@@ -182,7 +209,7 @@ namespace AccountRegistration
             this.Controls.Add(this.lastName);
             this.Controls.Add(this.program);
             this.Controls.Add(this.studentNo);
-            this.Name = "FrmRegistration";
+            this.Name = "Form1";
             this.Text = "FrmRegistration";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -208,6 +235,7 @@ namespace AccountRegistration
         private System.Windows.Forms.TextBox studentNoBox;
         private System.Windows.Forms.TextBox programBox;
         private System.Windows.Forms.TextBox addressBox;
+        private System.Windows.Forms.Button nextButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,10 @@ namespace AccountRegistration
         {
 
         }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
