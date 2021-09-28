718d715eb72c13e3c857555666b9555b1a69b5dc
Jerome <josejeromelalunio@gmail.com>
2021-09-29 05:27:20 +0800

    add student info class

---
+++ Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+public delegate long DelegateNumber(long number);
+public delegate long DelegateText(string text);
+
+public class StudentInfoClass
+{
+	string FirstName = "";
+	string LastName = "";
+	string MiddleName = "";
+	string Address = "";
+	string Program = "";
+
+	long Age = 0;
+	long ContactNo = 0;
+	long StudentNo = 0;
+
+	public StudentInfoClass()
+	{
+	}
+
+    public static string GetFirstName(string Firstname)
+    {
+		return Firstname;
+    }
+    public static string GetLastName(string Firstname)
+    {
+		return Firstname;
+    }
+    public static string GetMiddleName(string Firstname)
+    {
+		return Firstname;
+    }
+    public static string GetAddress(string Firstname)
+    {
+		return Firstname;
+    }
+    public static string GetProgram(string Firstname)
+    {
+		return Firstname;
+    }
+    public static string GetAge(string Firstname)
+    {
+		return Firstname;
+    }
+    public static string GetContactNo(string Firstname)
+    {
+		return Firstname;
+    }
+    public static string GetStudentNo(string Firstname)
+    {
+		return Firstname;
+    }
+}
